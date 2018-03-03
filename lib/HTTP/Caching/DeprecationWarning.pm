1f7761fca71e110377fd10f9e05a327917413613
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2018-03-03 17:11:24 +0100

    hide redundant deprecation warnings during test with ':hide'

--- lib/HTTP/Caching/DeprecationWarning.pm
+++ lib/HTTP/Caching/DeprecationWarning.pm
@@ -7,6 +7,13 @@ use feature 'state';
 
 use Carp;
 
+my $hide_deprecation_warning;
+
+sub import {
+    $hide_deprecation_warning = 1 if $_[1] and $_[1] eq ':hide';
+    __PACKAGE__->show_once();
+}
+
 sub show_once {
     my $class = shift;
     state $did_show_deprecation_warning;
@@ -20,6 +27,8 @@ sub show {
     my $class = shift;
     
     return if
+        $hide_deprecation_warning
+        or
         $ENV{HTTP_CACHING_DEPRECATION_WARNING_HIDE};
 
     my $deprecation_warning = q{
